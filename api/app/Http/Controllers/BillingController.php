9b99a0741e3cd0c0a60553fdaef075d23dc2dfcb
Muhammad Atiqi Hazizan <aqi852000@gmail.com>
2025-02-21 13:14:00 +0800

    Deploy frontend: Fri Feb 21 13:14:00 +08 2025

--- api/app/Http/Controllers/BillingController.php
+++ api/app/Http/Controllers/BillingController.php
@@ -4,6 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+defined('STATUS_TRANSITIONS', [
+    1 => [2],   
+    2 => [3, 8,9,10],
+    3 => [4],
+    4 => [5],
+    5 => [6, 8,9,10],
+    6 => [7],
+    7 => [],
+]);
+defined('STEPS', [
+    'Draft',                                // 1
+    'Waiting for HOD Approval',             // 2
+    'Waiting for Finance Review',           // 3
+    'Waiting for Finance Verification',     // 4
+    'Waiting for Finance Approval',         // 5
+    'Process Payment',                      // 6
+    'Paid',                                 // 7
+    'Rejected',                             // 8
+    'Returned',                             // 9
+    'Cancelled',                            // 10
+]);
+
+// Status transition rules
+// {
+//     1: [2],           // Draft -> Approval HOD
+//     2: [3, 8],        // Approval HOD -> Checking Finance, Rejected
+//     3: [4],           // Checking Finance -> Approval Finance
+//     4: [5, 8],        // Verify Finance -> Verified, Rejected
+//     5: [6, 8],        // Approval Finance[6, 8],        // Approval Finance -> Approved, Rejected] -> Approved, Rejected
+//     6: [7],           // Approved -> Paid[6, 8],        // Approval Finance -> Approved, Rejected]
+//     7: [],            // Paid -> (no further transitions)[6, 8],        // Approval Finance -> Approved, Rejected]
+//     8: []             // Rejected -> (no further transitions)
+//   };
+  
 class BillingController extends Controller
 {
     /**
