0cbfca89e925ebf39287e5e4d442817638b52930
Muhammad Atiqi Hazizan <aqi852000@gmail.com>
2025-02-24 12:36:10 +0800

    Deploy frontend: Mon Feb 24 12:36:10 +08 2025

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,74 +4,78 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
-  css: {
-    preprocessorOptions: {
-      css: {
-        javascriptEnabled: true,
-      },
-    },
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  build: {
-    outDir: '../api/public/dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Split vendor chunks
-          "vendor-react": ["react", "react-dom"],
-          "vendor-ui": [
-            "@headlessui/react",
-            "@heroicons/react",
-            "@tailwindcss/forms",
-            "lucide-react",
-            "tw-elements",
-          ],
-          query: ["axios"],
-          "components": ["recharts"],
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === 'production';
+  
+  return {
+    plugins: [react()],
+    base: isProduction ? '/dist/' : '/',
+    css: {
+      preprocessorOptions: {
+        css: {
+          javascriptEnabled: true,
         },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-        // assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          
-          if (ext === 'svg') {
-            if (assetInfo.name.includes('keenicons')) {
-              return 'assets/icons/[name]-[hash][extname]';
-            }
-            return 'assets/svg/[name]-[hash][extname]';
-          }
-          if (ext === 'css') {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          if (/\.(png|jpe?g|gif|ico)$/.test(assetInfo.name)) {
-            return 'assets/images/[name]-[hash][extname]';
-          }
-          if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
-            return 'assets/fonts/[name]-[hash][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
+      },
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
       },
     },
-    // Enable chunk size warnings at 500kb
-    chunkSizeWarningLimit: 500,
-    // Enable gzip compression
-    minify: "esbuild",
-    sourcemap: false,
-  },
-  server: {
-    proxy: {
-      '/api': '/api', // Sesuaikan dengan URL Laravel
-      // '/api': 'http://mbiclicks.mbikedah.com.my/api', // Sesuaikan dengan URL Laravel
+    build: {
+      outDir: '../api/public/dist',
+      assetsDir: 'assets',
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            "vendor-react": ["react", "react-dom"],
+            "vendor-ui": [
+              "@headlessui/react",
+              "@heroicons/react",
+              "@tailwindcss/forms",
+              "lucide-react",
+              "tw-elements",
+            ],
+            query: ["axios"],
+            "components": ["recharts"],
+          },
+          chunkFileNames: (chunkInfo) => {
+            return isProduction ? 'assets/js/[name]-[hash].js' : 'assets/js/[name].js';
+          },
+          entryFileNames: (assetInfo) => {
+            return isProduction ? 'assets/js/[name]-[hash].js' : 'assets/js/[name].js';
+          },
+          assetFileNames: (assetInfo) => {
+            const info = assetInfo.name.split('.');
+            const ext = info[info.length - 1];
+            const prefix = isProduction ? '/dist/' : '';
+            
+            if (ext === 'svg') {
+              if (assetInfo.name.includes('keenicons')) {
+                return prefix + 'assets/icons/[name]-[hash][extname]';
+              }
+              return prefix + 'assets/svg/[name]-[hash][extname]';
+            }
+            if (ext === 'css') {
+              return prefix + 'assets/css/[name]-[hash][extname]';
+            }
+            if (/\.(png|jpe?g|gif|ico)$/.test(assetInfo.name)) {
+              return prefix + 'assets/images/[name]-[hash][extname]';
+            }
+            if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
+              return prefix + 'assets/fonts/[name]-[hash][extname]';
+            }
+            return prefix + 'assets/[name]-[hash][extname]';
+          }
+        },
+      },
+      chunkSizeWarningLimit: 500,
+      minify: "esbuild",
+      sourcemap: false,
     },
-  },
+    server: {
+      proxy: {
+        '/api': '/api',
+      },
+    },
+  };
 });
